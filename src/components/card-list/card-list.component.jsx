df8c7f0cae61eb08fdaba3e5acf334dd17d65884
Terrance <terrance888@proton.me>
2022-06-12 20:48:37 -0400

    finish monsters rolodex with class components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,25 @@
 import { Component } from "react";
 
+import Card from "../card/card.component";
+
+import './card-list.styles.css';
+
 class CardList extends Component {
+
+
+
     render() {
         const { monsters } = this.props;
 
-        console.log(this.props.monsters);
-        console.log('render from CardList');
-        
         return (
-            <div>
+            <div className="card-list">
                 {monsters.map(monster => {
-                    return <h1 key={monster.id}>{monster.name}</h1>
+                    return (
+                        <Card
+                            monster={monster}
+                            key={monster.id}
+                        />
+                    )
                 })}
             </div>
         )
